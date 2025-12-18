1f6a35723fbb58bc621c910648591984f6eeb70c
HP-ELITEBOOK <HP-ELITEBOOK@DESKTOP-23R9HU2>
2025-12-18 10:26:03 +0530

    Initial commit

---
+++ Java_Code/src/com/tnsif/basics/MyFirstProgram.java
@@ -0,0 +1,10 @@
+package com.tnsif.basics;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ Java_Code/src/com/tnsif/opps/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.opps.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(100);
+		obj.setName("Aishwarya");
+		obj.setAge(20);
+		System.out.println(obj);
+
+	}
+	
+}
